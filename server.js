45f3d4d83c8b8cb7f0aaa8e9b959aa99a7ff42d2
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-16 11:56:01 +1000

    Adding code in server.js for mongoosxe conection

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// set up Mongoosewhen app starts, mondoDB find and connect to the DB if exists or create one
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// log mongo queries being executed
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
