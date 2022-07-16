6e5f57ea5e6a7ff3c4e33c2d75e42ed3a4d6a063
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-16 19:57:18 +1000

    comment out public in servre.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// for future development
+// app.use(express.static('public'));
 
 app.use(require('./routes'));
 
