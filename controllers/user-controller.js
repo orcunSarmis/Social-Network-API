4b0f26feb9c22e24e888b7f8b721bdc85f0ae05a
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-16 20:28:26 +1000

    adding delete user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,5 @@
 // Import User model
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 
 const userController ={
@@ -16,6 +16,7 @@ getAllUsers(req, res) {
         res.status(400).json(err);
     });
 },
+
 // get one user by id GET ?api?users/:id
 getUserById({ params }, res) {
     User.findOne({ _id: params.id })
@@ -39,15 +40,17 @@ getUserById({ params }, res) {
         res.status(400).json(err);
     });
 },
+
 // Create a user by POST  
 createUser({ body }, res){
     User.create(body)
     .then(dbUserData => res,json(dbUserData))
     .catch(err => res.status(400).json(err));
 },
+
 // update user by id PUT /api/users/:id
 updateUser({ params, body }, res) {
-    User.findOneAndUpdate({ _id:params.id }, body, { new: true })
+    User.findOneAndUpdate({ _id:params.id }, body, { new: true, runValidators: true })
     .then(dbUserData => {
         if (!dbUserData) {
             res.status(404).json({ message: 'No user found with this id!' });
@@ -58,6 +61,18 @@ updateUser({ params, body }, res) {
     .catch(err => res.status(400).json(err));
 },
 
+// delete user by id /api/users/:id
+deleteUser({ params}, res) {
+    User.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+},
 }
 
 module.exports = userController;
