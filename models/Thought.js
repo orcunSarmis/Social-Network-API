2bde151473f3e5712d79d0c0a1c9a7979d61cef3
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-16 23:13:59 +1000

    adding code of thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -30,4 +30,39 @@ const ThoughtSchema  = new Schema(
 }
 );
 
-const 
+const ReactionSchema = new Schema (
+    {
+         // set custom id to avoid confusion with parent thought _id
+         reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+         },
+         reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+
+         },
+         userName: {
+            type: String,
+            required: true
+         },
+         createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+         },
+         toJSON: {
+            getters: ture
+         }
+    }
+);
+
+// virtual to return amount of reactions to comment
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
