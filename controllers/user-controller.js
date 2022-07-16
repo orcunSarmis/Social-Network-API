1b8a6a62e94493fa67105b3e9e4b958a5c4831ec
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-16 20:58:02 +1000

    adding dlete friend method

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -73,6 +73,45 @@ deleteUser({ params}, res) {
     })
     .catch(err => res.status(400).json(err));
 },
-}
+
+// add friend to user list
+addFriend({ params }, res) {
+    User.findOneAndUpdate({ _id: params.userId }, { $push: { friends: params.friendId }},
+        {new: true, runValidators: false}
+        )
+.then(dbUserData => {
+    if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+    }
+    res.json(dbUserData);
+}) 
+.catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+})
+},
+
+// delete friend
+deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+        { _id: params.userId },
+         // Here, using the MongoDB $pull operator to remove the specific friend from the friends
+        { $pull: { friends: params.firendId }},
+        { new: true }
+    )
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+ }
+};
 
 module.exports = userController;
