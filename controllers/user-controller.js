79123b777f72d9884cc9e5c9c92d422a457a9862
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-16 15:25:24 +1000

    adding get user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,6 +16,29 @@ getAllUsers(req, res) {
         res.status(400).json(err);
     });
 },
+// get one user by id GET ?api?users/:id
+getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+    .populate({ 
+        path: 'thoughts',
+        select: '-__v'
+    })
+    .populate({ 
+        path: 'friends',
+        select: '-__v'
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+},
 // Create a user by POST  
 createUser({ body }, res){
     User.create(body)
@@ -23,3 +46,5 @@ createUser({ body }, res){
     .catch(err => res.status(400).json(err));
 }
 }
+
+module.exports = userController;
