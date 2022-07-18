90604e5f23c76cd6066a97ab34cff35acf7204a8
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-18 17:22:29 +1000

    working on error

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,34 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema (
+    {
+         // set custom id to avoid confusion with parent thought _id
+         reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+         },
+         reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+
+         },
+         userName: {
+            type: String,
+            required: true
+         },
+         createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+         },
+         toJSON: {
+            getters: true
+         }
+    }
+);
+
 const ThoughtSchema  = new Schema(
     {
         thoughtText: {
@@ -28,34 +56,6 @@ const ThoughtSchema  = new Schema(
     },
     id: false
 }
-);
-
-const ReactionSchema = new Schema (
-    {
-         // set custom id to avoid confusion with parent thought _id
-         reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-         },
-         reactionBody: {
-            type: String,
-            required: true,
-            maxLength: 280
-
-         },
-         userName: {
-            type: String,
-            required: true
-         },
-         createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-         },
-         toJSON: {
-            getters: ture
-         }
-    }
 );
 
 // virtual to return amount of reactions to comment
