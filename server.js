079425ea13551d10b9b2a622fe265018b687d709
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-07-19 23:04:06 +1000

    working on error

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
 
 // set up Mongoosewhen app starts, mondoDB find and connect to the DB if exists or create one
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
